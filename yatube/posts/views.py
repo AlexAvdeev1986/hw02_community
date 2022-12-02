8eb8a7c2285f6028e8149ddb95730e9b4bbd586a
AlexAvdeev1986 <ea703557@gmail.com>
2022-12-02 05:47:13 +0300

    11111
    22222

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import get_object_or_404, render
-
-from .models import Group, Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 NUM_OF_PUBLICATIONS: int = 10
 
 
 def index(request):
+    template = 'posts/index.html'
     posts = Post.objects.all()[:NUM_OF_PUBLICATIONS]
     context = {
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
+def group_list(request, slug):
+    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:NUM_OF_PUBLICATIONS]
+    posts = Post.objects.filter(group=group)[:NUM_OF_PUBLICATIONS]
     context = {
+        'text': slug,
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
